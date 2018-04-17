ab1f068b47f84802b2036a2b052a59cb8502c758
WllY23 <arias1533@gmail.com>
2018-04-16 20:56:03 -0500

    pag 13 de lectura 28

---
+++ ProyecZombie/Assets/Example3.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example3 : MonoBehaviour 
+{
+
+
+	void Start () 
+	{
+		
+	}
+	
+
+	void Update () 
+	{
+		
+	}
+}
