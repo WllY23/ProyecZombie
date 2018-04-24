4c23cffa4e7a399f4492fea7aeba36446bc9f36f
WllY23 <arias1533@gmail.com>
2018-04-24 13:44:03 -0500

    comensando la lect 29
    empezando la lectura 29

---
+++ ProyecZombie/Assets/Monster2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster2 : MonoBehaviour
+{
+	public float spacing;
+	public int id;
+	void Start () 
+	{
+		Debug.Log ("Im alive!");
+	}
+
+	void Update ()
+	{
+		float wave = Mathf.Sin (Time.fixedTime + id);
+		transform.position = new Vector3 (id * spacing, wave, 0);
+	}
+
+}
---
+++ ProyecZombie/Assets/MonsterSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour 
+{
+	public PrimitiveType primitiveType;
+	GameObject obj;
+
+	void Start () 
+	{
+		obj = gameObject.CreatePrimitive (primitiveType);
+	}
+
+	void Update () 
+	{
+	}
+		
+	public enum PrimitiveType
+	{
+		Sphere, Capsule, Cilinder, Cube, Plane
+	}
+}
